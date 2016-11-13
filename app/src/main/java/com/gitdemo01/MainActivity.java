b0122943c48eb15a0b19e0464f23f410ca049cc6
User1 <user1@user.com>
2016-11-13 16:00:58 +0530

    Comment 1 in Folder3

--- app/src/main/java/com/gitdemo01/MainActivity.java
+++ app/src/main/java/com/gitdemo01/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//Comment 1
+
 
         // Comment Added.
         // Hello Task 4
